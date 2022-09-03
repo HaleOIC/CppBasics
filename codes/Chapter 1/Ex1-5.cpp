f65c6c795eaa4fd636448aca97f04666594a74ac
Hale <3116227229@qq.com>
2022-09-03 14:07:38 +0800

    finish chapter 1.1 1.2 execises

---
+++ codes/Chapter 1/Ex1-5.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+	int v1,v2;
+	std::cout<<"Enter two numbers:"<<std::endl;
+	std::cin>>v1>>v2;
+	std::cout<<(v1*v2);
+	std::cout<<std::endl;
+	return 0;
+}
---
+++ codes/Chapter 1/Ex1-6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(){
+	int v1,v2;
+	std::cin>>v1 >> v2;
+	std::cout<< "The sum of"<< v1;
+			 << " and "<< v2;
+			 << "is "<< v1+v2<< std::endl;
+
+	return 0;
+}
+/*
+It can not be exacuated,because the first line is illegal.
+It does not return an object,for the syntax ;
+*/
