07d69f7d5a0bb97f7a88cd41f98a212dbb7f6ab5
Hale <3116227229@qq.com>
2022-09-03 22:29:20 +0800

    finish some codes and notes

---
+++ codes/Chapter 2/Ex2-1.cpp
@@ -0,0 +1,7 @@
+/*
+The defferences between the int,  long , long long, short 
+is the space they will need.
+and unsigned type can not present the minus number 
+while having more space to represent the number which is bigger than the sigend type.
+double has more currency than the float.
+*/
---
+++ codes/Chapter 2/Ex2-2.cpp
@@ -0,0 +1,6 @@
+/*
+absolutely we need more accurency in the mortgage payment.
+for rate and principal  and payment we need the double 
+if it needs more accurency, it may use the long double.
+since they do not want to cultivate any errors.
+*/
