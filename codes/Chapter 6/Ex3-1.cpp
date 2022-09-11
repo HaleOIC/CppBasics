d8e4a001eb874f479ed405274237c4ffe63af8c0
Hale <3116227229@qq.com>
2022-09-11 21:17:56 +0800

    delete some files

--- codes/Chapter 6/Ex3-1.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using std::cin; using std::cout; using std::endl;
-int main(){
-	int v1, v2;
-	cin >> v1 >> v2;
-	cout << v1 << v2 << endl;
-	return 0;
-}
--- codes/Chapter 6/Ex3-11.cpp
+++
@@ -1,9 +0,0 @@
-#include<string>
-#include<iostream>
-using namespace std;
-int main(){
-	const string s = "Keep out!";
-	for (auto &c : s) {/* ... */}
-	return 0;
-}
-/* 合法 c 的类型是 const char &*/
